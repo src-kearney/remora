b6b246fd947cbe1aeb21fbe455df96dc9f092d59
Sean Kearney <src.kearney@gmail.com>
2026-03-21 14:31:09 -0700

    feat: full lowering pipeline to LLVM and JIT compile

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -1,3 +1,5 @@
+#include "mlir/ExecutionEngine/ExecutionEngine.h"
+#include "mlir/ExecutionEngine/MemRefUtils.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/DialectRegistry.h"
@@ -8,10 +10,23 @@
 #include "stablehlo/dialect/Register.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetSelect.h"
 #include "mlir/InitAllExtensions.h"
 #include "mlir/Dialect/Linalg/Passes.h"
 #include "mlir/Transforms/Passes.h"
 #include "stablehlo/conversions/linalg/transforms/Passes.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Conversion/ReconcileUnrealizedCasts/ReconcileUnrealizedCasts.h"
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Conversion/Passes.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/Transforms/Passes.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -27,6 +42,8 @@ int main(int argc, char **argv) {
   mlir::registerAllDialects(registry);
   mlir::stablehlo::registerAllDialects(registry);
   mlir::registerAllExtensions(registry);
+  mlir::registerBuiltinDialectTranslation(registry);
+  mlir::registerLLVMDialectTranslation(registry);
 
   /// https://github.com/llvm/llvm-project/blob/f46a5153850c1303d687233d4adf699b01041da8/mlir/include/mlir/IR/MLIRContext.h#L41
   /// MLIRContext is the top-level object for a collection of MLIR operations. It
@@ -63,11 +80,37 @@ int main(int argc, char **argv) {
   pm.addPass(mlir::createInlinerPass());
   pm.addPass(mlir::createLinalgElementwiseOpFusionPass());
 
+  mlir::bufferization::OneShotBufferizePassOptions bufOpts;
+  bufOpts.bufferizeFunctionBoundaries = true;
+
+  /// lower to LLVM dialect
+  pm.addPass(mlir::bufferization::createOneShotBufferizePass(bufOpts)); // tensor → memref (incl. return types)
+  pm.addNestedPass<mlir::func::FuncOp>(mlir::createConvertLinalgToLoopsPass()); // linalg → scf
+  pm.addPass(mlir::createLowerAffinePass());
+  pm.addPass(mlir::createSCFToControlFlowPass());
+  pm.addPass(mlir::createConvertControlFlowToLLVMPass());
+  pm.addPass(mlir::createArithToLLVMConversionPass());
+  pm.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
+  pm.addPass(mlir::createConvertFuncToLLVMPass());
+  pm.addPass(mlir::createReconcileUnrealizedCastsPass());
+
   if (mlir::failed(pm.run(*module))) {
     llvm::errs() << "Pass pipeline failed\n";
     return 1;
   }
 
-  module->print(llvm::outs());
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  auto maybeEngine = mlir::ExecutionEngine::create(*module);
+  if (!maybeEngine) {
+    llvm::handleAllErrors(maybeEngine.takeError(), [](const llvm::ErrorInfoBase &e) {
+      llvm::errs() << "Failed to create ExecutionEngine: " << e.message() << "\n";
+    });
+    return 1;
+  }
+  auto &engine = maybeEngine.get();
+
+  /// TODO invoke on function (probably `main`, whatever func module->print outputs)
   return 0;
 }
